c1766eb74859130cc14dc69fa44566629e0f6549
Valery Colong <valery.colong@activspaces.com>
2019-06-03 06:05:34 +0100

    Added example project to package. Remove use of console.log in examples in favour or print.

---
+++ example/main.dart
@@ -0,0 +1,24 @@
+import 'package:vcard/vcard.dart';
+
+main() {
+  ///Create a new vCard
+  var vCard = VCard();
+
+  ///Set properties
+  vCard.firstName = 'FirstName';
+  vCard.middleName = 'MiddleName';
+  vCard.lastName = 'LastName';
+  vCard.organization = 'ActivSpaces Labs';
+  vCard.photo.attachFromUrl('https://www.activspaces.com/wp-content/uploads/2019/01/ActivSpaces-Logo_Dark.png', 'PNG');
+  vCard.workPhone = 'Work Phone Number';
+  vCard.birthday = DateTime.now();
+  vCard.title = 'Software Developer';
+  vCard.url = 'https://github.com/valerycolong';
+  vCard.note = 'Notes on contact';
+
+  /// Save to file
+  vCard.saveToFile('./contact.vcf');
+
+  /// Get as formatted string
+  print(vCard.getFormattedString());
+}
